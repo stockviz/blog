f3fa7bb45774bf1b3d5351789f6118cb18ff2012
Shyam Sunder <shyam.udupa@gmail.com>
2018-10-15 11:28:13 +0530

    cleanup

--- allocation/2-asset.R
+++ allocation/2-asset.R
@@ -34,81 +34,55 @@ asset1Px<-sqlQuery(lcon, sprintf("select time_stamp, px_close from bhav_index wh
 asset2Name<-"0_5"
 asset2Px<-sqlQuery(lcon, sprintf("select time_stamp, tri from INDEX_CCIL_TENOR where index_name = '%s' and time_stamp >= '%s' and time_stamp <= '%s'", asset2Name, startDate, endDate))
 
+allXts<-merge(xts(asset1Px[,2], as.Date(asset1Px[,1])), xts(asset2Px[,2], as.Date(asset2Px[,1])))
+names(allXts)<-c('PX_A1', 'PX_A2')
+allXts[,2]<-na.locf(allXts[,2])
+allXts<-na.omit(allXts)
+
 runScenario<-function(assetWeights, rebalThreshold, stt=0, tax=0){
-	allXts<-merge(xts(asset1Px[,2], as.Date(asset1Px[,1])), xts(asset2Px[,2], as.Date(asset2Px[,1])))
-	names(allXts)<-c('PX_A1', 'PX_A2')
+	allSubsetXts<-allXts
+	assetCols<-1:2
+	assetVals<-3:4
+	holder<-matrix(0L, nrow=length(allSubsetXts[,1]), ncol=4)
+	
+	holder[1,assetCols]<-assetWeights*initialInvestment/allSubsetXts[1,]
+	holder[1,assetVals]<-assetWeights*initialInvestment
 
-	allXts[,2]<-na.locf(allXts[,2])
-	allXts<-na.omit(allXts)
 
-	allXts$A1<-0.0
-	allXts$A2<-0.0
-
-	allXts$A1_VAL<-0.0
-	allXts$A2_VAL<-0.0
-
-	allXts$A1[1] <- assetWeights[1]*initialInvestment/allXts[1,1]
-	allXts$A2[1] <- assetWeights[2]*initialInvestment/allXts[1,2]
-
-	allXts$A1_VAL[1] <- assetWeights[1]*initialInvestment
-	allXts$A2_VAL[1] <- assetWeights[2]*initialInvestment
-
-	allDf<-data.frame(allXts)
-
-	for(i in 2:length(allDf[,1])) {
-		currentVal1 <- allDf[i,1] * allDf$A1[i-1]
-		currentVal2 <- allDf[i,2] * allDf$A2[i-1]
-		totalVal <- currentVal1 + currentVal2
-		
-		currentWt1<- currentVal1/totalVal
-		currentWt2<- currentVal2/totalVal
-		
-		if(currentWt1 >= (1+rebalThreshold) * assetWeights[1]) {
-			#rebal
-			#print(sprintf("1: %d", i))
-			
-			targetVal1 <- assetWeights[1] * totalVal
-			excessAmt <- (currentVal1 - targetVal1)*(1-stt)*(1-tax)
-			
-			excessAsset1 <- excessAmt/allDf[i,1]
-			excessAsset2 <- excessAmt/allDf[i,2]
-			
-			allDf$A1[i] <- allDf$A1[i-1] - excessAsset1
-			allDf$A2[i] <- allDf$A2[i-1] + excessAsset2
-			allDf$A1_VAL[i] <- currentVal1 - excessAmt
-			allDf$A2_VAL[i] <- currentVal2 + excessAmt
-		} else if (currentWt2 >= (1+rebalThreshold) * assetWeights[2]) {
-			#rebal
-			#print(sprintf("2: %d", i))
-			
-			targetVal2 <- assetWeights[2] * totalVal
-			excessAmt <- (currentVal2 - targetVal2)*(1-stt)*(1-tax)
-			
-			excessAsset1 <- excessAmt/allDf[i,1]
-			excessAsset2 <- excessAmt/allDf[i,2]
-			
-			allDf$A1[i] <- allDf$A1[i-1] + excessAsset1
-			allDf$A2[i] <- allDf$A2[i-1] - excessAsset2
-			allDf$A1_VAL[i] <- currentVal1 + excessAmt
-			allDf$A2_VAL[i] <- currentVal2 - excessAmt
-		} else {
-			allDf$A1[i]<-allDf$A1[i-1]
-			allDf$A2[i]<-allDf$A2[i-1]
-			allDf$A1_VAL[i]<-currentVal1
-			allDf$A2_VAL[i]<-currentVal2
+	for(i in 2:length(allXts[,1])) {
+		currentVal <- as.numeric(allSubsetXts[i,] * holder[i-1, assetCols])
+		totalVal <- sum(currentVal)
+		currentWt<- currentVal/totalVal
+		if(all(currentWt < (1+rebalThreshold) * assetWeights)){
+			#no rebal required
+			holder[i,assetCols]<-holder[i-1,assetCols]
+			holder[i,assetVals]<-currentVal
+			next
 		}
+		
+		targetVal <- assetWeights * totalVal
+		excessAmt <- (currentVal - targetVal)
+		
+		sttOnExcess <- sum(stt*abs(excessAmt))
+		taxOnProfits <- sum(tax*excessAmt[excessAmt > 0])
+		totalDrag <- sttOnExcess+taxOnProfits
+		
+		excessAsset <- as.numeric(excessAmt/allSubsetXts[i,])
+		
+		holder[i,assetCols] <- holder[i-1,assetCols] - excessAsset - as.numeric(totalDrag/2/allSubsetXts[i,])
+		holder[i,assetVals] <- currentVal - excessAmt - totalDrag/2
 	}
 
-	allDf$TOTAL <- allDf$A1_VAL + allDf$A2_VAL
-	resXts<-xts(allDf[, c('PX_A1', 'PX_A2', 'TOTAL')], as.Date(row.names(allDf)))
-	retXts<-merge(dailyReturn(resXts[,1]), dailyReturn(resXts[,2]), dailyReturn(resXts$TOTAL))
-	names(retXts)<-c('PX_A1', 'PX_A2', 'ALLOC')
+	allSubsetXts$TOTAL <- rowSums(holder[,assetVals])
 
+	retXts<-merge(dailyReturn(allSubsetXts[,1]), dailyReturn(allSubsetXts[,2]), dailyReturn(allSubsetXts$TOTAL))
+	names(retXts)<-c('A1', 'A2', 'ALLOC')
+	#browser()
 	return(retXts)
 }
 
 runAnalysis<-function(){
-	thresholdPct<-seq(0.05, 0.2, 0.05)
+	thresholdPct<-c(c(0.05, 0.1), seq(0.2, 1, 0.2))
 	aAllocPct<-seq(0.4, 0.9, 0.1)
 
 	allResultDf<-data.frame(matrix(ncol=4,nrow=0))
@@ -164,7 +138,8 @@ createTable1<-function(){
 	toPrint<- retDf %>%
 		group_by(THRESH) %>%
 		summarize(NO_TAX_MEAN = mean(CUMULATIVE[DRAG==0]), TAX_MEAN = mean(CUMULATIVE[DRAG==1])) %>%
-		mutate(DIFF=TAX_MEAN/NO_TAX_MEAN) 
+		mutate(DIFF=TAX_MEAN/NO_TAX_MEAN - 1) %>%
+		arrange(desc(THRESH)) 
 		
 	toPrintDf<-data.frame(toPrint)
 	toPrintDf<-round(100*toPrintDf, 2)
@@ -172,32 +147,8 @@ createTable1<-function(){
 	tt2<-arrangeGrob(tableGrob(toPrintDf, rows=NULL, theme=mytheme), ncol=1, 
 				top = textGrob(sprintf("Avg. Returns by threshold and tax difference"),gp=gpar(fontsize=12, fontfamily='Segoe UI')), 
 				bottom=textGrob("@StockViz", gp=gpar(fontsize=10, col='grey', fontfamily='Segoe UI')))
-	ggsave(sprintf('%s/table.cumulative.2-asset.avg.tax-diff.png', reportPath), tt2, width=5, height=3, units='in')
+	ggsave(sprintf('%s/table.cumulative.2-asset.png', reportPath), tt2, width=5, height=3, units='in')
 }
-
-createTable1B<-function(){
-	pdf(NULL)
-	retDf<-read.csv(sprintf('%s/cumulative.2-asset.csv', reportPath))
-	
-	options(tibble.print_max = Inf)
-	toPrint<- retDf %>%
-		group_by(A_ALLOC, THRESH) %>%
-		summarize(NO_TAX = mean(CUMULATIVE[DRAG==0]), TAX = mean(CUMULATIVE[DRAG==1])) %>%
-		mutate(DIFF=TAX/NO_TAX-1) %>%
-		arrange(desc(DIFF)) %>%
-		print()
-	
-	options(tibble.print_max = 10)
-	
-	toPrintDf<-data.frame(toPrint)
-	toPrintDf<-round(100*toPrintDf, 2)
-	
-	tt2<-arrangeGrob(tableGrob(toPrintDf, rows=NULL, theme=mytheme), ncol=1, 
-				top = textGrob(sprintf("Returns sorted by tax difference"),gp=gpar(fontsize=12, fontfamily='Segoe UI')), 
-				bottom=textGrob("@StockViz", gp=gpar(fontsize=10, col='grey', fontfamily='Segoe UI')))
-	ggsave(sprintf('%s/table.cumulative.2-asset.avg.tax-diff.B.png', reportPath), tt2, width=5, height=8, units='in')
-}
-
 
 createTable2<-function(aa){
 	pdf(NULL)
@@ -210,12 +161,12 @@ createTable2<-function(aa){
 	toPrintDf$THRESH<-round(toPrintDf$THRESH*100.0, 0)
 	
 	tt2<-arrangeGrob(tableGrob(toPrintDf, rows=NULL, theme=mytheme), ncol=1, 
-				top = textGrob(sprintf("Returns by threshold and tax %.0f%%", 100*aa),gp=gpar(fontsize=12, fontfamily='Segoe UI')), 
+				top = textGrob(sprintf("%.0f%% A1 %.0f%% A2", 100*aa, 100*(1-aa)),gp=gpar(fontsize=12, fontfamily='Segoe UI')), 
 				bottom=textGrob("@StockViz", gp=gpar(fontsize=10, col='grey', fontfamily='Segoe UI')))
-	ggsave(sprintf('%s/table.cumulative.2-asset.png', reportPath, 100*aa), tt2, width=3, height=4, units='in')
+	ggsave(sprintf('%s/table.cumulative.2-asset.%.0f.png', reportPath, 100*aa), tt2, width=3, height=6, units='in')
 }
 
 #runAnalysis()
-#createTable2(0.6)
+createTable2(0.6)
 #createTable1()
-createTable1B()
+#createTable1B()
