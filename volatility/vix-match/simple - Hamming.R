031f321b7334ea2ab127e10b5122480c448864a9
shyams80 <shyam.udupa@gmail.com>
2021-11-27 10:28:09 +0530

    hamming

---
+++ volatility/vix-match/simple - Hamming.R
@@ -0,0 +1,80 @@
+source("D:/stockviz/r/config.r")
+reportPath <- "D:/StockViz/public/blog/volatility/vix-match"
+library('RODBC')
+library('tidyverse')
+library('reshape2')
+library('ggthemes')
+library('quantmod')
+library('PerformanceAnalytics')
+library('lubridate')
+library('ggpubr')
+library('grid')
+library('gridExtra')
+library('gtable')
+
+source("D:/StockViz/public/blog/common/misc.R")
+
+options(stringsAsFactors = FALSE)
+options("scipen"=100)
+pdf(NULL)
+
+segLength <- 20
+
+lcon <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, ldbname, ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
+
+vixDf <- sqlQuery(lcon, "select time_stamp, px_close from vix_history")
+vixXts <- xts(vixDf[,2], vixDf[,1])
+
+vixXts <- dailyReturn(vixXts)
+vixXts <- vixXts[-1]
+vixXts <- ifelse(vixXts > 0, 1, 0)
+
+lastX <- tail(vixXts, segLength)
+
+distXts <- rollapply(vixXts, segLength, function(X) sum(coredata(X) != coredata(lastX)), by.column=F)
+
+distDf <- data.frame(distXts)
+distDf$T <- index(distXts)
+
+smlDist <- head(distDf[order(distDf),])
+names(smlDist) <- c('DIST', 'T')
+tt1<-arrangeGrob(tableGrob(smlDist, rows=NULL, theme=tableTheme), ncol=1, top=textGrob("India VIX Hamming Distance", gp=gpar(fontsize=12, fontface ='bold', fontfamily='Segoe UI')), bottom=textGrob("@StockViz", gp=gpar(fontsize=10, col='grey', fontfamily='Segoe UI')))
+
+ggsave(sprintf("%s/closest-hamming.png", reportPath), tt1, width=3, height=nrow(smlDist)*0.5, units='in')
+
+matchEnd <- as.Date("2010-11-02")
+
+toPlotXts <- vixXts[sprintf("%s/%s", matchEnd-100, matchEnd+100),]
+names(toPlotXts) <- c('VIX')
+toPlotXts$C <- 0
+toPlotXts[sprintf("%s/%s", index(first(tail(vixXts[sprintf("/%s", matchEnd)], segLength))), matchEnd),]$C <- 1
+
+toPlot <- data.frame(toPlotXts)
+toPlot$T <- index(toPlotXts)
+
+p1 <- ggplot(toPlot, aes(x=T, y=VIX)) +
+	theme_economist() +
+	geom_line(aes(color=C), size=1) +
+	guides(color=F) +
+	scale_x_date(breaks='1 month') +
+	labs(x='', y='VIX', color='', title="", subtitle=sprintf("[%s:%s]", first(index(toPlotXts)), last(index(toPlotXts)))) 
+	
+toPlotXts <- vixXts[sprintf("%s/%s", last(index(lastX))-200, last(index(lastX))+200),]
+names(toPlotXts) <- c('VIX')
+toPlotXts$C <- 0
+toPlotXts[sprintf("%s/%s", index(first(tail(vixXts[sprintf("/%s", last(index(lastX)))], segLength))), last(index(lastX))),]$C <- 1
+
+toPlot <- data.frame(toPlotXts)
+toPlot$T <- index(toPlotXts)
+
+p2 <- ggplot(toPlot, aes(x=T, y=VIX)) +
+	theme_economist() +
+	geom_line(aes(color=C), size=1) +
+	guides(color=F) +
+	scale_x_date(breaks='1 month') +
+	labs(x='', y='VIX', color='', title="", subtitle=sprintf("[%s:%s]", first(index(toPlotXts)), last(index(toPlotXts)))) 
+	
+figure <- ggarrange(p1, p2, ncol=1, nrow=2)	
+annotate_figure(figure, top = text_grob('INDIA VIX', face = "bold", size = 14, family='Segoe UI'), bottom = text_grob("@StockViz", face="bold", size=12, family="Segoe UI", color='grey'))
+	
+ggsave(sprintf("%s/closest-match-hamming.png", reportPath), width=16, height=14, units="in")
