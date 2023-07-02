428dc2af7ac5b40c13d9a2f285588ffe3d491891
shyams80 <shyam.udupa@gmail.com>
2023-07-02 11:15:03 +0530

    index ATH

---
+++ ATH/script_us.R
@@ -0,0 +1,78 @@
+library('RODBC')
+library('quantmod')
+library('PerformanceAnalytics')
+
+library('tidyverse')
+library('lubridate')
+library('reshape2')
+library('ggthemes')
+library('patchwork')
+library('viridis')
+
+library('knitr')
+library('kableExtra')
+
+options("scipen"=100)
+options(stringsAsFactors = FALSE)
+
+source("d:/stockviz/r/config.r")
+
+reportPath <- "."
+
+lcon <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, ldbname, ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
+lconUs2 <- odbcDriverConnect(sprintf("Driver={ODBC Driver 17 for SQL Server};Server=%s;Database=%s;Uid=%s;Pwd=%s;", ldbserver, "StockVizUs2", ldbuser, ldbpassword), case = "nochange", believeNRows = TRUE)
+
+indexNames <- c("SPY", "QQQ")
+
+indexPx <- NULL
+for(iName in indexNames){
+	iDf <- sqlQuery(lconUs2, sprintf("select C, time_stamp from BHAV_EQ_TD where SYMBOL = '%s'", iName))
+	indexPx <- merge.xts(indexPx, xts(iDf[,1], iDf[,2]))
+}
+names(indexPx) <- indexNames
+
+startDate <- first(index(indexPx))
+
+for(j in 1:ncol(indexPx)){
+	inrIndexPx <- na.omit(indexPx[,j])
+	inrRollMax <- inrIndexPx[inrIndexPx == cummax(inrIndexPx)]
+	inrIndexRet <- dailyReturn(inrIndexPx)
+	
+	athDates <- index(inrRollMax)
+	athDatesDf <- data.frame(D1=athDates, D2=c(athDates[2:length(athDates)], NA))
+	athDatesDf$DIFF <- as.numeric(athDatesDf$D2 - athDatesDf$D1)
+	
+	inrAthDatesTb <- athDatesDf %>% arrange(desc(DIFF)) %>% filter(DIFF > 20)
+	inrAthDatesTb$MAX_DD <- NA
+	
+	for(k in 1:nrow(inrAthDatesTb)){
+		inrAthDatesTb$MAX_DD[k] <- round(maxDrawdown(inrIndexRet[paste0(inrAthDatesTb$D1[k], '/', inrAthDatesTb$D2[k])]) * 100, 2)
+	}
+	
+	### plots
+	
+	### time between ATH
+	kable(inrAthDatesTb, caption = sprintf('%s Days between All Time Highs', indexNames[j]), booktabs = T) %>% 
+			kable_styling(bootstrap_options = 'striped', full_width = F) %>% 
+			save_kable(file=sprintf("%s/%s-ath-drawdowns.png", reportPath, indexNames[j]), bs_theme = 'flatly')
+			
+	### price ATH
+	tpXts <- merge(log(inrIndexPx), log(inrRollMax))
+	names(tpXts) <- c(indexNames[j], "ATH")
+	toPlot <- data.frame(tpXts)
+	toPlot$T <- index(tpXts)
+	jName <- colnames(toPlot)[1]
+	
+	ggplot(toPlot, aes(x=T, y=.data[[jName]])) +
+		theme_economist() +
+		theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+		geom_line(color='darkgrey') +
+		geom_point(aes(x=T, y=ATH), color='darkgreen') +
+		scale_x_date(date_labels = "%Y", date_breaks="1 year") +
+		labs(y='log(index)', x='', title=sprintf('%s All Time Highs (INR)', indexNames[j]), subtitle=sprintf("[%s:%s]", first(index(tpXts)), last(index(tpXts)))) +
+		annotate("text", x=last(index(tpXts)), y=min(tpXts[,1]), label = "@StockViz", hjust='right', vjust='bottom', col="white", cex=6, fontface = "bold", alpha = 0.8)
+		
+	ggsave(sprintf("%s/%s-ath-price.png", reportPath, indexNames[j]), width=12, height=6, units="in")
+
+	
+}
