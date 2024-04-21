3586719e9eeab8d72cb6f7365df830074c3d9131
shyams80 <shyam.udupa@gmail.com>
2024-04-21 11:32:23 +0530

    weekly momentum rebal scenarios

---
+++ momentum/rebal-frequency/combine-weekly.R
@@ -0,0 +1,64 @@
+library('quantmod')
+library('PerformanceAnalytics')
+library('PortfolioAnalytics')	
+
+library('tidyverse')
+library('lubridate')
+
+options("scipen"=100)
+options(stringsAsFactors = FALSE)
+
+source("D:/StockViz/public/blog/common/plot.common.R")
+
+reportPath <- "."
+drag <- 0.5/100
+
+load(sprintf("%s/symRets-weekly.Rdata", reportPath)) #symRets
+
+symRets$RET <- as.numeric(symRets$RET)
+symRets$OVERLAP <- as.numeric(symRets$OVERLAP)
+
+stratNames <- unique(symRets$STRATEGY)
+symXts <- do.call(merge.xts, lapply(stratNames, function(X) xts(symRets[symRets$STRATEGY == X,]$RET, symRets[symRets$STRATEGY == X,]$PERIOD_END)))
+names(symXts) <- stratNames
+
+symXts2 <- do.call(merge.xts, lapply(stratNames, function(X) {
+	rets <- xts(symRets[symRets$STRATEGY == X, c('RET', 'OVERLAP')], symRets[symRets$STRATEGY == X,]$PERIOD_END)
+	rets$RET_ADJ <- rets$RET - (1 - rets$OVERLAP)*drag
+	rets$RET_ADJ
+	}))
+	
+names(symXts2) <- stratNames
+
+statsDf <- data.frame(SharpeRatio.annualized(symXts))
+statsDf <- rbind(statsDf, Return.annualized(symXts)*100)
+statsDf <- data.frame(t(statsDf))
+colnames(statsDf) <- c('SHARPE', 'RET')
+statsDf$ID <- row.names(statsDf)
+write.csv(statsDf, file=sprintf("%s/symStatsAll-weekly.gross.csv", reportPath), row.names = F)
+
+statsDf <- data.frame(SharpeRatio.annualized(symXts2))
+statsDf <- rbind(statsDf, Return.annualized(symXts2)*100)
+statsDf <- data.frame(t(statsDf))
+colnames(statsDf) <- c('SHARPE', 'RET')
+statsDf$ID <- row.names(statsDf)
+write.csv(statsDf, file=sprintf("%s/symStatsAll-weekly.net.csv", reportPath), row.names = F)
+
+
+Common.PlotCumReturns(symXts[, setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.gross.png", reportPath), NULL)
+Common.PlotCumReturns(symXts["/2019", setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.pre.gross.png", reportPath), NULL)
+Common.PlotCumReturns(symXts["2020-05-01/", setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.post.gross.png", reportPath), NULL)
+
+Common.PlotCumReturns(symXts2[, setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.net.png", reportPath), NULL)
+Common.PlotCumReturns(symXts2["/2019", setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.pre.net.png", reportPath), NULL)
+Common.PlotCumReturns(symXts2["2020-05-01/", setdiff(stratNames, c("BENCH"))], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-weekly.post.net.png", reportPath), NULL)
+
+for(sName in stratNames){
+	if (sName == 'BENCH') next
+	toPlot <- merge(symXts[, sName], symXts2[, sName], symXts[, 'BENCH'])
+	Common.PlotCumReturns(toPlot, "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-%s-weekly.png", reportPath, sName), NULL)
+	Common.PlotCumReturns(toPlot["/2019",], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-%s-weekly.pre.png", reportPath, sName), NULL)
+	Common.PlotCumReturns(toPlot["2020-05-01/",], "Momentum", "weekly rebal-frequency", sprintf("%s/symRets-%s-weekly.post.png", reportPath, sName), NULL)
+}
+
+
