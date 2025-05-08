fbca47f79b8773e8a77f6c8f00f1ad1369462fd7
shyam <shyam.sunder@stockviz.biz>
2025-05-08 10:40:54 +0530

    blog

--- us-etf-returns/etf.vs.spy.R
+++ us-etf-returns/etf.vs.spy.R
@@ -23,13 +23,22 @@ lconUs2 <- odbcDriverConnect(
   case = "nochange", believeNRows = TRUE
 )
 
-minDt <- sqlQuery(lconUs2, "select min(time_stamp) from bhav_eq_td where symbol='SPY'")[[1]]
-maxDt <- sqlQuery(lconUs2, "select max(time_stamp) from bhav_eq_td where symbol='SPY'")[[1]]
+startDate <- as.Date("2020-05-01")
+endDate <- Sys.Date()
+
+minDt <- sqlQuery(lconUs2, sprintf("select min(time_stamp) from bhav_eq_td where symbol='SPY' and time_stamp >= '%s'", startDate))[[1]]
+maxDt <- sqlQuery(lconUs2, sprintf("select max(time_stamp) from bhav_eq_td where symbol='SPY' and time_stamp <= '%s'", endDate))[[1]]
+
 etfSymbols <- sqlQuery(lcon, sprintf("select symbol, fund from etf_meta where INCEPTION_DATE <= '%s'", minDt))
-etfSymbols <- rbind(etfSymbols, c('BRK/B', 'Berkshire Hathaway Inc Class B'))
+#etfSymbols <- rbind(etfSymbols, c('BRK/B', 'Berkshire Hathaway Inc Class B'))
 
 etfs <- sqlQuery(lconUs2, sprintf("select symbol, min(time_stamp) minTs, max(time_stamp) maxTs from bhav_eq_td 
-                               where symbol in ('%s') group by symbol", paste(etfSymbols$symbol, collapse="','")))
+                                  where symbol in ('%s') 
+                                  and time_stamp >= '%s' and time_stamp <= '%s'
+                                  group by symbol", 
+                                  paste(etfSymbols$symbol, collapse="','"),
+                                  minDt,
+                                  maxDt))
 
 etfs <- etfs |> filter(minTs == minDt & maxTs == maxDt)
 
@@ -59,7 +68,8 @@ annSharpeDf$SYMBOL <- row.names(annSharpeDf)
 
 annDf <- annRetDf |> 
   inner_join(annSharpeDf, join_by(SYMBOL)) |> 
-  inner_join(etfSymbols |> mutate(symbol = gsub('/', '.', symbol)), join_by(SYMBOL == symbol)) |>
+  #inner_join(etfSymbols |> mutate(symbol = gsub('/', '.', symbol)), join_by(SYMBOL == symbol)) |>
+  inner_join(etfSymbols, join_by(SYMBOL == symbol)) |>
   relocate(c(SYMBOL, fund), 1) |>
   arrange(desc(ANN_RET))
 
@@ -80,55 +90,14 @@ annDf %>%
              'fund' = '',
              'ANN_RET' = 'Returns',
              'ANN_SHARPE' = 'Sharpe') %>%
-  gtsave(sprintf("%s/etf.annual.returns.html", reportPath))
+  gtsave(sprintf("%s/etf.annual.returns.%s-%s.html", reportPath, minDt, maxDt))
 
 webshot2::webshot(
-  sprintf("%s/etf.annual.returns.html", reportPath),
-  sprintf("%s/etf.annual.returns.png", reportPath)
+  sprintf("%s/etf.annual.returns.%s-%s.html", reportPath, minDt, maxDt),
+  sprintf("%s/etf.annual.returns.%s-%s.png", reportPath, minDt, maxDt)
 )
 
 
 ###############################
 
-startDateStr <- "2009-01-01/"
 
-spyRet <- as.numeric(Return.annualized(dXts[startDateStr,'SPY']))*100
-spySharpe <- as.numeric(SharpeRatio.annualized(dXts[startDateStr,'SPY']))
-
-annRetDf <- data.frame(t(data.frame(Return.annualized(dXts[startDateStr,])*100)))
-colnames(annRetDf) <- c('ANN_RET')
-annRetDf$SYMBOL <- row.names(annRetDf)
-
-annSharpeDf <- data.frame(t(data.frame(SharpeRatio.annualized(dXts[startDateStr,]))))
-colnames(annSharpeDf) <- c('ANN_SHARPE')
-annSharpeDf$SYMBOL <- row.names(annSharpeDf)
-
-annDf <- annRetDf |> 
-  inner_join(annSharpeDf, join_by(SYMBOL)) |> 
-  inner_join(etfSymbols |> mutate(symbol = gsub('/', '.', symbol)), join_by(SYMBOL == symbol)) |>
-  relocate(c(SYMBOL, fund), 1) |>
-  arrange(desc(ANN_RET))
-
-annDf %>%
-  gt() %>%
-  tab_header(title = "Annual Returns (post-GFC)", subtitle = sprintf('%s%s', startDateStr, maxDt)) %>%
-  tab_footnote(footnote = md("<sub><sub>*@StockViz*</sub></sub>")) %>%
-  fmt_number(decimals=2) %>%
-  opt_stylize(style=5) %>%
-  data_color(columns = ANN_RET,
-             fn = \(arr) ifelse(arr < spyRet, 'red', ifelse(arr > spyRet, 'darkgreen', 'black')), 
-             apply_to = 'text') %>%
-  data_color(columns = ANN_SHARPE,
-             fn = \(arr) ifelse(arr < spySharpe, 'red', ifelse(arr > spySharpe, 'darkgreen', 'black')), 
-             apply_to = 'text') %>%
-  tab_options(table.font.size = '130%') %>%
-  cols_label('SYMBOL' = '',
-             'fund' = '',
-             'ANN_RET' = 'Returns',
-             'ANN_SHARPE' = 'Sharpe') %>%
-  gtsave(sprintf("%s/etf.annual.returns.post-GFC.html", reportPath))
-
-webshot2::webshot(
-  sprintf("%s/etf.annual.returns.post-GFC.html", reportPath),
-  sprintf("%s/etf.annual.returns.post-GFC.png", reportPath)
-)
